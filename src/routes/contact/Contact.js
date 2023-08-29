4c116a2c25a0f9edef41fec79171806514ad81d2
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2023-08-29 19:10:51 +0400

    added contact form with validations

--- src/routes/contact/Contact.js
+++ src/routes/contact/Contact.js
@@ -1,12 +1,14 @@
+import { useEffect } from "react";
 import { Footer } from "../../components";
 import ContactForm from "./ContactForm";
 
 const Contact = () => {
-  document.title = "Contact | Sophia Martinez";
+  useEffect(() => {
+    document.title = "Contact | Sophia Martinez";
+  })
   return (
     <article className="contact-main">
       <h2 className="title-font pink-text h2-tag">Contact Me</h2>
-      <br></br>
       <p className="white-text p-tag">
         If you are interested to hire me I am always open to new opportunities
         and interesting projects! Feel free to contact me if you have any other
